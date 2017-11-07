ac72f30ef0416266d12bcea7e8c8220783905ab9
Davide Mendolia <davide@gokarumi.com>
2017-11-07 09:53:27 +0100

    Add changed based on swift 4.1

--- Sources/main.swift
+++ Sources/main.swift
@@ -55,14 +55,14 @@ struct NuclearOption {
 
     func attackMonadic() -> Impacted? {
         return arm().flatMap { nuke in
-            aim().flatMap { target in
-                launch(target: target, nuke: nuke)
+            self.aim().flatMap { target in
+                self.launch(target: target, nuke: nuke)
             }
         }
     }
 }
-
-/// Variant of Either Monad commonly Used in Swift, called Result
+//
+///// Variant of Either Monad commonly Used in Swift, called Result
 import Result
 
 struct NuclearResult {
@@ -97,6 +97,87 @@ struct NuclearResult {
             aim().flatMap { target in
                 launch(target: target, nuke: nuke)
             }
+        }
+    }
+}
+
+/// We will only define map and flatMap for our needs in this context
+/// We are using the naming convention of the Optional Monad defined in the standard library
+
+protocol Monad {
+    associatedtype Value
+    associatedtype F: Monad
+
+    /// Using initializer as equivalent of pure
+    init(value: Value)
+    func flatMap<U>(_ transform: (Value) throws -> F) rethrows -> F where F.Value == U
+    func map<U>(_ transform: (Value) throws -> U) rethrows -> F where F.Value == U
+}
+
+extension Optional: Monad {
+    typealias F = Optional
+
+    init(value: Value) {
+        self = .some(value)
+    }
+//    func map<U>(_ transform: (Wrapped) throws -> U) rethrows -> U? {
+//        switch self {
+//        case let .some(value):
+//            return .some(try transform(value))
+//        default:
+//            return .none
+//        }
+//    }
+}
+
+extension Result: Monad {
+    typealias F = Result
+
+    func flatMap<U>(_ transform: (Value) throws -> Result<U, Error>) rethrows -> Result<U, Error> {
+        switch self {
+        case let .success(value):
+            return try transform(value)
+        case let .failure(error):
+            return .failure(error)
+        }
+    }
+
+    func map<U>(_ transform: (Value) throws -> U) rethrows -> Result<U, Error> {
+        switch self {
+        case let .success(value):
+            return .success(try transform(value))
+        case let .failure(error):
+            return .failure(error)
+        }
+    }
+}
+
+
+struct NuclearMonad<NukeMonad: Monad, TargetMonad: Monad, AttackMonad: Monad>
+        where NukeMonad.Value == Nuke,
+        TargetMonad.Value == Target,
+        AttackMonad.Value == (Nuke, Target) {
+    func arm() -> NukeMonad {
+        return NukeMonad(value: Nuke())
+    }
+
+    func aim() -> TargetMonad {
+        return TargetMonad(value: Target())
+    }
+
+    func attackMonadic() -> AttackMonad {
+//        return arm().flatMap { (nuke: Nuke) in
+//            aim().map { (target:Target) -> (Nuke, Target) in
+//                return (nuke, target)
+//            }
+//        }
+
+        return arm().flatMap(aiming)
+    }
+
+    func aiming(nuke: Nuke) -> AttackMonad {
+        aim().map { (target:Target) -> (Nuke, Target) in
+            return (nuke, target)
         }
     }
 }
